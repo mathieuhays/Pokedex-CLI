2df868e342f942b8225682c9cbad79b7f721cec3
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-06-25 10:20:52 +0200

    fix compile error

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/mathieuhays/pokedex-cli/internal/pokeapi"
+	"time"
 )
 
 func main() {
 	fmt.Println("  ===  P O K E D E X  ===  ")
 
+	pokeClient := pokeapi.NewClient(time.Second * 2)
 	cfg := &config{
-		api: pokeapi.NewPokeAPI(),
+		api: &pokeClient,
 	}
 
 	consoleRepl := newConsoleRepl(getCommands(), cfg)
